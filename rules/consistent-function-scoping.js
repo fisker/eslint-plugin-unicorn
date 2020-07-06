6b7dae61ce8676bc1dbf797bb3edb83d47515fbc
fisker <lionkay@gmail.com>
2020-07-06 18:26:53 +0800

    Clean

--- rules/consistent-function-scoping.js
+++ rules/consistent-function-scoping.js
@@ -6,73 +6,6 @@ const getReferences = require('./utils/get-references');
 
 const MESSAGE_ID = 'consistent-function-scoping';
 
-const isSameScope = (scope1, scope2) =>
-	scope1 && scope2 && (scope1 === scope2 || scope1.block === scope2.block);
-
-function checkReferences(scope, parent, scopeManager) {
-	const hitReference = references => references.some(reference => {
-		if (isSameScope(parent, reference.from)) {
-			return true;
-		}
-
-		const {resolved} = reference;
-		const [definition] = resolved.defs;
-
-		// Skip recursive function name
-		if (definition && definition.type === 'FunctionName' && resolved.name === definition.name.name) {
-			return false;
-		}
-
-		return isSameScope(parent, resolved.scope);
-	});
-
-	const hitDefinitions = definitions => definitions.some(definition => {
-		const scope = scopeManager.acquire(definition.node);
-		return isSameScope(parent, scope);
-	});
-
-	// This check looks for neighboring function definitions
-	const hitIdentifier = identifiers => identifiers.some(identifier => {
-		// Only look at identifiers that live in a FunctionDeclaration
-		if (
-			!identifier.parent ||
-				identifier.parent.type !== 'FunctionDeclaration'
-		) {
-			return false;
-		}
-
-		const identifierScope = scopeManager.acquire(identifier);
-
-		// If we have a scope, the earlier checks should have worked so ignore them here
-		if (identifierScope) {
-			return false;
-		}
-
-		const identifierParentScope = scopeManager.acquire(identifier.parent);
-		if (!identifierParentScope) {
-			return false;
-		}
-
-		// Ignore identifiers from our own scope
-		if (isSameScope(scope, identifierParentScope)) {
-			return false;
-		}
-
-		// Look at the scope above the function definition to see if lives
-		// next to the reference being checked
-		return isSameScope(parent, identifierParentScope.upper);
-	});
-
-	return getReferences(scope)
-		.map(({resolved}) => resolved)
-		.filter(Boolean)
-		.some(variable =>
-			hitReference(variable.references) ||
-			hitDefinitions(variable.defs) ||
-			hitIdentifier(variable.identifiers)
-		);
-}
-
 // https://reactjs.org/docs/hooks-reference.html
 const reactHooks = new Set([
 	'useState',
@@ -130,10 +63,9 @@ function canMoveToUpperScope(node, scope) {
 	const variablesInParentScopeButNotInFunction = referencesInParentScopeButNotInFunction.map(({resolved}) => resolved);
 	const variables = references.map(({resolved}) => resolved);
 
-
 	return !variables.some(variable => {
 		if (!variable) {
-return false
+			return false
 		}
 
 		if (variablesInParentScopeButNotInFunction.includes(variable)) {
